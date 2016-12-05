374806463957036b1cc2f6de16f7a887758e148e
dadavis21 <dadavis21@yahoo.com>
2016-12-05 15:27:32 -0600

    sql syntax error

--- views.py
+++ views.py
@@ -115,7 +115,7 @@ def get_trip(username, keyword, lpnum):
     '''
 
     liked = bool(get_trip_user(keyword, username, lpnum)[3])
-    return render_template('webpage2/trip.html', trip=trip, coords=None, keyword=keyword, liked=liked)
+    return render_template('webpage2/trip.html', trip=trip, coords=None, keyword=keyword, liked=liked, username=username)
 
 
 @app.route('/<username>/search/<keyword>/<lpnum>/<like>')
