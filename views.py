796865104ce3c6c0d6467ee2551c37192c2ce215
root <root@fa16-cs411-47.cs.illinois.edu>
2016-12-05 17:50:48 -0600

    indentation

--- views.py
+++ views.py
@@ -374,8 +374,8 @@ def recommend_trip(username):
 		return None
 	else:
 		for trip_info in rv:
-            keyword = trip_info[0]
-            lpnum = trip_info[1]
+            		keyword = trip_info[0]
+            		lpnum = trip_info[1]
 			recommended[0].append("http://fa16-cs411-47.cs.illinois.edu/{}/search/{}/{}".format(username, keyword, lpnum + 1))
 			recommended[1].append(keyword + " {}".format(lpnum+1))
 
@@ -396,8 +396,8 @@ def past_trips(username):
 		return None
 	else:
 		for trip_info in rv:
-            keyword = trip_info[0]
-            lpnum = trip_info[1]
+            		keyword = trip_info[0]
+            		lpnum = trip_info[1]
 			past[0].append("http://fa16-cs411-47.cs.illinois.edu/{}/search/{}/{}".format(username, keyword, lpnum))
 			past[1].append(keyword + " {}".format(lpnum))
 			
