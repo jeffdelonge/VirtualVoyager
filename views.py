300194289f6bb81e2775183010827680dcfadaa2
dadavis21 <dadavis21@yahoo.com>
2016-12-06 00:45:05 -0600

    sql syntax error

--- views.py
+++ views.py
@@ -88,7 +88,7 @@ def get_trip(username, keyword, lpnum):
         coords.append(get_location_coords(location['name']))
 
     liked = get_trip_user(keyword, username, lpnum)[3]
-    return render_template('webpage2/trip.html', trip=trip, coords=None, keyword=keyword, liked=liked, username=username, lpnum=lpnum)
+    return render_template('webpage2/trip.html', trip=trip, coords=coords, keyword=keyword, liked=liked, username=username, lpnum=lpnum)
 
 
 @app.route('/<username>/search/<keyword>/<lpnum>/<like>')
