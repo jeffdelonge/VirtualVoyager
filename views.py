6c3b5efb398b1e6c1f4b90e80bab5d7badc72609
dadavis21 <dadavis21@yahoo.com>
2016-12-06 00:52:44 -0600

    sql syntax error

--- views.py
+++ views.py
@@ -83,7 +83,7 @@ def get_trip(username, keyword, lpnum):
     trip = get_trip_locations(keyword, lpnum)
     #raise Exception("GOT TRIP DICTS HERE: {}".format(trip))
     trip = [location for location in trip if location]
-    raise Exception("Trip length: {}".format(len(trips)))
+    raise Exception("Trip length: {}".format(len(trip)))
     coords = []
     for location in trip:
         coords.append(get_location_coords(location['name']))
